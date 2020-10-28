3d0a959271932f02a353fdd739a524a75fb07e7a
briansostek <bes140@pitt.edu>
2020-10-27 21:26:31 -0400

    added dot indicators to show slide count

--- webpage/script.js
+++ webpage/script.js
@@ -1,12 +1,16 @@
 var index=0;
+var slides= document.getElementsByClassName("slide");
+var dots= document.getElementsByClassName("dot");
+
 show();
 
 
 
 function show()
 {
+
+  showDots();
   var i;
-  var slides= document.getElementsByClassName("slide");
   for(i=0; i<slides.length; i++) //hides all slides
     slides[i].style.display="none";
   index++;
@@ -14,5 +18,17 @@ function show()
     index=0;
   slides[index].style.display="block";
   slides[index].currentTime=0;
-  setTimeout(show,slides[index].duration*1000);
+  if(slides[index].duration>0)
+    setTimeout(show,slides[index].duration*1000);
+  else {
+    setTimeout(show,10000);
+  }
+}
+
+function showDots()
+{
+  var i;
+  for(i=0; i<dots.length; i++)
+    dots[i].style.backgroundColor="#555";
+  dots[index].style.backgroundColor="white";
 }
