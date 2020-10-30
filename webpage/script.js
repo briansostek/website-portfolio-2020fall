6be0abf6230314122c169f9cee68797372d0114d
briansostek <bes140@pitt.edu>
2020-10-29 21:05:29 -0400

    Added fun typing effect and fixed lag of first video

--- webpage/script.js
+++ webpage/script.js
@@ -1,28 +1,38 @@
-var index=0;
+var index=1;
+var let=0;
 var slides= document.getElementsByClassName("slide");
 var dots= document.getElementsByClassName("dot");
-
-show();
+if(slides.length>0)
+  show();
+typeLine();
 
 
 
 function show()
 {
-
+  if(index>=slides.length)
+    index=0;
   showDots();
   var i;
   for(i=0; i<slides.length; i++) //hides all slides
     slides[i].style.display="none";
-  index++;
-  if(index>=slides.length)
-    index=0;
-  slides[index].style.display="block";
-  slides[index].currentTime=0;
-  if(slides[index].duration>0)
-    setTimeout(show,slides[index].duration*1000);
-  else {
-    setTimeout(show,10000);
-  }
+
+
+
+  let currSlide= slides[index];
+  currSlide.currentTime=0;
+  currSlide.style.display="block";
+
+    if(currSlide.duration!=null) //if it's a video
+    {
+      setTimeout(show,currSlide.duration*1000);
+    }
+    else {
+      setTimeout(show,10000); //otherwise show for 10 seconds.
+    }
+    index++;
+
+
 }
 
 function showDots()
@@ -31,4 +41,25 @@ function showDots()
   for(i=0; i<dots.length; i++)
     dots[i].style.backgroundColor="#555";
   dots[index].style.backgroundColor="white";
+}
+
+function typeLine()
+{
+  var text= "System.out.print(\"I am Brian Sostek\");"
+  title= document.getElementById("title");
+  title.innerHTML=text.substring(0,let);
+  if(let<text.length)
+  {
+    let++;
+    var typeTime= Math.random()*150+25;
+    setTimeout(typeLine,typeTime);
+  }
+  else {
+    setTimeout(function ()
+    {
+      title.innerHTML=text+"\nBrian Sostek";
+    }, 1000);
+
+  }
+
 }
